15d027fee2a25fbe5bde67bfbb12bee30b89c190
Mr.sun <18273965210@163.com>
2018-11-22 23:30:35 +0800

    add home

---
+++ src/view/home/course.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css'
+
+
+
+export default function(){
+    return (
+        <ul className="curriculum">
+            <li>
+
+            </li>
+        </ul>
+    )
+}
--- src/view/home/index.js
+++ src/view/home/index.js
@@ -2,19 +2,47 @@ import React, { Component,Fragment } from 'react';
 import Header from '../../comm/components/header'
 
 import Menu from '../../comm/components/menu'
+import Course from './course'
 
 
+
+let tabImg = [
+    require('../../comm/components/nav/img/banner.png'),
+    require('../../comm/components/nav/img/banner.png'),
+    require('../../comm/components/nav/img/banner.png'),
+    require('../../comm/components/nav/img/banner.png')
+]
 class Home extends Component {
     state = {  }
     render() { 
         return ( 
             <Fragment>
                 <Header {...{}}/>
-                {/* <Menu/> */}
-                <div className="banner">
-                    <ul className='picList'></ul>
-                    <nav className='picNav'></nav>
+                <Menu/>
+                <div className="page">
+                    <div className="banner tab">
+                        <ul className='picList'>
+                            {tabImg.map((elt,i)=>{
+                                return (
+                                    <li key={i}>
+                                        <img src={elt} alt=''/>
+                                    </li>
+                                )
+                            })}
+                        </ul>
+                        <nav className='picNav'>
+                            {
+                                tabImg.map((elt,i)=>{
+                                    return (
+                                        <span key={i} className={i===0?'active':''}></span>   
+                                    )
+                                })
+                            }
+                        </nav>
+                    </div>
+                    <Course/>
                 </div>
+                
             </Fragment>
             
         )
